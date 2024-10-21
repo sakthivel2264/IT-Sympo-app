5e42712f82360f8591771ffe935c2dbf153c59b6
Sakthivel <sakthivelpandiyan2264@gmail.com>
2024-10-21 10:46:17 +0530

    feat: updated loading

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,38 +1,19 @@
-import React, { useRef, useState, useEffect, Suspense } from 'react';
-import Loading from './ui/Loading'; // Assuming you have a Loading component
+import React from 'react';
 
 const Video: React.FC = () => {
-  const videoRef = useRef<HTMLVideoElement>(null);
-  const [showVideo, setShowVideo] = useState(false);
-
-  // Delay the video loading by 3 seconds (3000 ms)
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowVideo(true);
-    }, 6000); // Set delay time here (in ms)
-
-    return () => clearTimeout(timer); // Cleanup on component unmount
-  }, []);
-
   return (
-    <div className="flex justify-center items-center m-4  rounded-xl w-full h-72">
-      <Suspense fallback={<div className="flex justify-center items-center"><Loading /></div>}>
-        {showVideo ? (
-          <video
-            ref={videoRef}
-            loop
-            muted
-            playsInline
-            autoPlay
-            className="border rounded-xl w-full"
-          >
-            <source src="/sympo.mp4" type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-        ) : (
-          <Loading /> // Display loading until the video is shown
-        )}
-      </Suspense>
+    <div className="flex justify-center items-center  rounded-xl w-full h-72">
+      <video
+        loop
+        muted
+        playsInline
+        autoPlay
+        preload="auto" // This attribute ensures the video is loaded immediately
+        className="border rounded-xl w-full"
+      >
+        <source src="/sympo.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
     </div>
   );
 };
